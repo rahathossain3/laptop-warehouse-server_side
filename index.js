6dc1635cb6edcfc9007ad24d54396d378faee3f9
rahathossain3 <rahathossain210@gmail.com>
2022-05-05 08:45:01 +0600

    database connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //for dotenv
@@ -9,6 +10,23 @@ require('dotenv').config();
 //middleware-----------
 app.use(cors());
 app.use(express.json());
+
+//mongodb
+
+username: warehouse1
+password: FOB6Ql2uJTfpp87R
+
+
+
+const uri = "mongodb+srv://warehouse1:FOB6Ql2uJTfpp87R@cluster0.3aknj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+
+});
+
+
 
 
 // basic api ---------------
