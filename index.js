357118f35aa49c08091941686a0e8f281aabba72
rahathossain3 <rahathossain210@gmail.com>
2022-05-05 08:54:05 +0600

    database connect

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-
 //for dotenv
 require('dotenv').config();
 
+const app = express();
 
 //middleware-----------
 app.use(cors());
@@ -20,11 +21,26 @@ password: FOB6Ql2uJTfpp87R
 
 const uri = "mongodb+srv://warehouse1:FOB6Ql2uJTfpp87R@cluster0.3aknj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
 
-});
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('laptopwarehouse').collection('item');
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+
+// });
 
 
 
